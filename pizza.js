a6ccfc7e331a16e6c5c9b78783a1d714de15b6ff
Unknown <ksaetern6@mail.csuchico.edu>
2017-03-22 10:10:50 -0700

    Edited .js file

--- pizza.js
+++ pizza.js
@@ -1,7 +1,26 @@
-window.onload = main;
+window.onload = showValues;
 
-function main()
-{
-  console.log("Hello World!");
+function showValues(){
+  var veg = document.getElementById("checkVeggies");
+  //
+  var name = document.getElementById("orderName");
+  //
+  console.log("Finished running JavaScript");
+  console.log(name.value);
+
 }
+function getCustomerName(){
+  var name = document.getElementById("orderName");
 
+  var veg = document.getElementById("checkVeggies").checked;
+  var pep = document.getElementById("checkPep").checked;
+  var sasuage = document.getElementById("checkSasuage").checked;
+
+  if(veg==true){
+    console.log("You got a veggie pizza");
+  }
+  else if(veg==true&&pep==true&&sasuage==true){
+    console.log("You got a supreme pizza")
+  }
+  console.log("Order received by " + name.value);
+}
